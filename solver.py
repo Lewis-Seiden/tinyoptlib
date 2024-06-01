956146883356641dcf1b0d3a24f8723f51de15b7
Lewis-Seiden <lewy@seiden.us>
2024-05-31 21:03:47 -0700

    separate demo and solver into separate files

--- solver.py
+++ solver.py
@@ -1,12 +1,8 @@
 from enum import Enum
 import casadi as ca
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patch
-import matplotlib.animation as anim
 
 import math
-
 
 class Waypoint:
     def __init__(self, x: float | None, y: float | None, heading: float | None, resolution: int, guess: bool):
@@ -185,8 +181,12 @@ def generate_swerve_trajectory(
                 interval[1] - interval[0]))
 
     # debug plot
+    timestamps = [0]
+    for i in range(1, len(problem.debug.value(dt, problem.initial()))):
+        timestamps.append(sum(problem.debug.value(dt, problem.initial())[0:i]))
     initial = {
         "dt": problem.debug.value(dt, problem.initial()),
+        "timestamp": timestamps,
         "x":  problem.debug.value(x, problem.initial()),
         "y":  problem.debug.value(y, problem.initial()),
         "vx": problem.debug.value(vx, problem.initial()),
@@ -194,25 +194,6 @@ def generate_swerve_trajectory(
         "ax": problem.debug.value(ax, problem.initial()),
         "ay": problem.debug.value(ay, problem.initial()),
     }
-    print(initial)
-
-    fig = plt.figure()
-    ax1 = plt.subplots()[1]
-
-    ax1.set_title("initial guess xy")
-    ax1.scatter(initial["x"], initial["y"], c=np.linspace([1,0,0],[0,0,1],num=len(initial["x"])))
-
-    dts = initial["dt"]
-    timestamps = [0]
-    for i in range(1, len(dts)):
-        timestamps.append(sum(dts[0:i]))
-
-    ax2 = plt.subplots()[1]
-    ax2.set_xticks(timestamps)
-    ax2.set_title("initial guess x")
-    ax2.twinx().scatter(timestamps, initial["x"], color="black")
-    ax2.twinx().scatter(timestamps, initial["vx"], color="red")
-    ax2.twinx().scatter(timestamps, initial["ax"], color="pink")
 
     problem.solver("ipopt")
     solve = problem.solve()
@@ -230,83 +211,6 @@ def generate_swerve_trajectory(
         "omega": solve.value(omega),
         "ax": solve.value(ax),
         "ay": solve.value(ay),
-        "alpha": solve.value(alpha)
+        "alpha": solve.value(alpha),
+        "initial": initial
     }
-
-
-# test
-bumper_trans = [(-0.5, 0.5), (0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
-test = generate_swerve_trajectory(
-    [Waypoint(1.0, 0.0, 0.0, 10, False), Waypoint(0.75, 0.2, None, 20, False), Waypoint(1.0, 1.0, 3.14, 20, True), 
-     Waypoint(None, 1.5, 3.14, 20, False), Waypoint(2.0, 1.0, 0.0, 20, False)
-     ],
-    bumper_trans,
-    [(-0.4, 0.4), (0.4, 0.4), (0.4, -0.4), (-0.4, -0.4)],
-    20.0,
-    5.0,
-    0.05,
-    4.0,
-    50.0,
-    [
-        # (0, 2, False, Constraint.MAX_TRANS_VEL, 0.0), (0, 2, True, Constraint.MAX_TRANS_VEL, 0.25), (1, 1, True, Constraint.VEL_DIRECTION, -3.14)
-        ]
-)
-
-print(test)
-
-fig = plt.figure()
-ax_xy = plt.subplots()[1]
-
-ax_xy.set_title("path xy")
-ax_xy.plot(test["x"], test["y"])
-ax_xy.scatter(test["x"], test["y"])
-
-dts = test["dt"]
-timestamps = [0]
-for i in range(1, len(dts)):
-    timestamps.append(sum(dts[0:i]))
-
-ax_x = plt.subplots()[1]
-ax_x.set_xticks(timestamps)
-ax_x.twinx().plot(timestamps, test["x"], color="black")
-ax_x.twinx().plot(timestamps, test["vx"], color="red")
-ax_x.twinx().plot(timestamps, test["ax"], color="pink")
-
-afig, ax_anim = plt.subplots()
-ax_anim.set_title("anim traj")
-ax_anim.plot(test["x"], test["y"])
-box = patch.Rectangle((test["x"][0] - bumper_trans[1][0], test["y"][0] - bumper_trans[1][1]), bumper_trans[1][0] * 2, bumper_trans[1][1] * 2, angle=test["theta"][0] * 180.0 / math.pi, rotation_point='center', fill=False)
-ax_anim.add_patch(box)
-framerate = 60.0
-def lerp (a, b, t):
-    return a + ((b - a) * t)
-mod_frame = 0
-def update_anim(frame):
-    global mod_frame
-    mod_frame += 1
-    mod_frame = mod_frame % (((test["timestamp"][-1]) * framerate) - 1)
-    time = mod_frame/framerate
-    s = 0
-    s_err = math.inf
-    for i, timestamp in enumerate(test["timestamp"]):
-        if abs(time - timestamp) <= s_err:
-            s_err = abs(time - timestamp)
-            s = i
-    if test["timestamp"][s] > time:
-        s -= 1
-    t = (time - test["timestamp"][s]) / (test["timestamp"][s + 1] - test["timestamp"][s])
-    # print("time " + '{0:.2f}'.format(time) + "; s stamp " + '{0:.2f}'.format(test["timestamp"][s]) + "; frame " + '{0:.0f}'.format(mod_frame) + "; sample " + str(s) + "; t " + '{0:.2f}'.format(t))
-    box.set_xy((lerp(test["x"][s], test["x"][s + 1], t) - bumper_trans[1][0], lerp(test["y"][s], test["y"][s + 1], t) - bumper_trans[1][1]))
-    box.set_angle(lerp(test["theta"][s], test["theta"][s + 1], t) * 180.0 / math.pi)
-    ax_anim.add_patch(box)
-    return
-
-traj_anim = anim.FuncAnimation(fig=afig, 
-                         func=update_anim,
-                                frames=len(test["x"]), interval=1000/framerate, blit=False)
-
-ax_dt = plt.subplots()[1]
-ax_dt.set_title("dts")
-ax_dt.plot(test["timestamp"], test["dt"])
-
-plt.show()
